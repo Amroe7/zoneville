07a1be38c92566fc73cb423a827c1315962d55c3
Amroe7 <muhamad.amroe@gmail.com>
2021-04-15 15:14:34 +0700

    Project Pengayaan

---
+++ resources/views/students/create.blade.php
@@ -0,0 +1,99 @@
+@extends('students.layout')
+
+
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add Student</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('students.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+
+
+<form action="{{ route('students.store') }}" method="POST">
+    @csrf
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NIS:</strong>
+                <input type="text" name="nis" class="form-control" placeholder="nis">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="nama" class="form-control" placeholder="nama">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jenis Kelamin:</strong>
+                <input type="text" name="jns_kelamin" class="form-control" placeholder="jns_kelamin">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tempat Lahir:</strong>
+                <input type="text" name="temp_lahir" class="form-control" placeholder="temp_lahir">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tanggal Lahir:</strong>
+                <input type="text" name="tgl_lahir" class="form-control" placeholder="tgl_lahir">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                <input type="text" name="alamat" class="form-control" placeholder="alamat">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Asal Sekolah:</strong>
+                <input type="text" name="asal_sekolah" class="form-control" placeholder="asal_sekolah">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kelas:</strong>
+                <input type="text" name="kelas" class="form-control" placeholder="kelas">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jurusan:</strong>
+                <input type="text" name="jurusan" class="form-control" placeholder="jurusan">
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+
+</form>
+
+@endsection
---
+++ resources/views/students/edit.blade.php
@@ -0,0 +1,101 @@
+@extends('students.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Edit Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('students.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<form action="{{ route('students.update',$student->id) }}" method="POST">
+    @csrf
+    @method('PUT')
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NIS:</strong>
+                <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nama:</strong>
+                    <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Jenis Kelamin:</strong>
+                        <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Tempat Lahir:</strong>
+                            <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="form-group">
+                                <strong>Tanggal Lahir:</strong>
+                                <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Alamat:</strong>
+                                    <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Asal Sekolah:</strong>
+                                        <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <div class="form-group">
+                                            <strong>Kelas:</strong>
+                                            <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-12 col-md-12">
+                                            <div class="form-group">
+                                                <strong>Jurusan:</strong>
+                                                <input type="text" name="name" value="{{ $student->name }}" class="form-control" placeholder="Name">
+                                            </div>
+                                        </div>
+
+
+                                        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </div>
+
+</form>
+
+@endsection
